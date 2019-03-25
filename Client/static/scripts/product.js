8c5dda4eb40cc121230c601ae9130ebecba5e17e
Hudson Burns Prestidge <hudson.prestidge@gmail.com>
2019-03-26 03:29:52 +1300

    add contact page, populate products page

--- Client/static/scripts/product.js
+++ Client/static/scripts/product.js
@@ -1,4 +1,14 @@
 window.addEventListener('load', function (e) {
+    const products = Array.from(document.getElementsByClassName('product-window'));
+    products.forEach(function (product) {
+        let addButton = product.querySelector('.add-product-button');
+        product.addEventListener('mouseenter', function (e) {
+            addButton.style.animation = 'slideIn 500ms forwards';
+        });
+        product.addEventListener('mouseleave', function (e) {
+            addButton.style.animation = 'slideOut 500ms forwards';
+        });
+    });
     getProducts();
 });
 const getProducts = function () {
--- Client/static/scripts/product.ts
+++ Client/static/scripts/product.ts
@@ -1,8 +1,20 @@
 window.addEventListener('load', function(e) {
+  const products :Element[] = Array.from(document.getElementsByClassName('product-window'))
+  products.forEach(function(product) :void {
+    let addButton = <HTMLElement>product.querySelector('.add-product-button')
+
+    product.addEventListener('mouseenter', function(e) :void {
+      addButton.style.animation = 'slideIn 500ms forwards'
+    })
+
+    product.addEventListener('mouseleave', function(e) :void {
+      addButton.style.animation = 'slideOut 500ms forwards'
+    })
+  })
   getProducts()
 })
 
-const getProducts = function() {
+const getProducts = function() :void{
   const getProd = new XMLHttpRequest()
   getProd.open('GET', '/api/v1/products')
   getProd.onload = function() {
