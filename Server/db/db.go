d097196b7aae17c1fa4ac500ab32b2e291a53b03
Hudson Burns Prestidge <hudson.prestidge@gmail.com>
2019-04-04 04:04:35 +1300

    logging in creates user session

--- Server/db/db.go
+++ Server/db/db.go
@@ -6,9 +6,11 @@ import(
   _ "github.com/lib/pq"
   "net/http"
   "encoding/json"
+  // "encoding/base64"
   "strings"
   "fmt"
   "golang.org/x/crypto/bcrypt"
+  "crypto/rand"
 )
 
 type Product struct {
@@ -99,7 +101,6 @@ func AddUser() http.HandlerFunc {
 
 func AuthenticateUser() http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
-      log.Printf("attempted login")
       r.ParseForm()
       username := strings.Join(r.Form["username"], "")
       password := strings.Join(r.Form["password"], "")
@@ -109,12 +110,18 @@ func AuthenticateUser() http.HandlerFunc {
       if err != nil {
         log.Printf("?", err)
       }
-      query := fmt.Sprintf("SELECT * FROM users WHERE username='%s'", username)
-      rows, err := db.Query(query)
-      defer rows.Close()
+      tx, err := db.Begin()
+      defer tx.Commit()
       if err != nil {
         log.Printf("?", err)
       }
+      query := fmt.Sprintf("SELECT * FROM users WHERE username='%s'", username)
+      rows, err := tx.Query(query)
+
+      if err != nil {
+        log.Printf("?", err)
+      }
+      var currentUser User
       for rows.Next() {
         var (
           id int
@@ -126,14 +133,42 @@ func AuthenticateUser() http.HandlerFunc {
         if err != nil {
           log.Printf("?", err)
         }
-        u := User{Id: id, Username: username, Passwordhash: passwordhash, Isdisabled: isdisabled}
-        err = bcrypt.CompareHashAndPassword([]byte(u.Passwordhash), []byte(password))
+        currentUser = User{Id: id, Username: username, Passwordhash: passwordhash, Isdisabled: isdisabled}
+      }
+      rows.Close()
+      err = bcrypt.CompareHashAndPassword([]byte(currentUser.Passwordhash), []byte(password))
+      if err != nil {
+        log.Printf("password doesn't match")
+      } else {
+        randomString, err := generateRandomString(50)
         if err != nil {
-          log.Printf("password doesn't match")
-        } else {
-          log.Printf("login successful")
+          log.Printf("failed to generate a session key")
+        }
+        log.Printf("session key: %s", randomString)
+        formattedStatement := fmt.Sprintf("INSERT INTO usersessions(sessionkey, userid, logintime, lastseentime) VALUES('%s', %d, NOW(), NOW())", randomString, currentUser.Id)
+        log.Printf(formattedStatement)
+        stmt, err := tx.Prepare(formattedStatement)
+        if err != nil {
+          log.Printf("?", err)
+        }
+        _, err = stmt.Exec()
+        if err != nil {
+          log.Printf("?", err)
         }
       }
       http.Redirect(w, r, "/index.html", 303)
     }
+}
+
+func generateRandomString(n int) (string, error) {
+  bytes := make([]byte, 50)
+  _, err := rand.Read(bytes)
+  if err != nil {
+    return "", err
+  }
+  const letters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  for i, b := range bytes {
+    bytes[i] = letters[b%byte(len(letters))]
+  }
+  return string(bytes), nil
 }
--- Server/db/migrations/3_add_usersessions_table.up.sql
+++ Server/db/migrations/3_add_usersessions_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE usersessions (
   sessionkey TEXT PRIMARY KEY,
   userid INTEGER NOT NULL REFERENCES users(id),
-  logintime TIMESTAMP NOT NULL,
-  lastseentime TIMESTAMP NOT NULL
+  logintime TIMESTAMPTZ NOT NULL,
+  lastseentime TIMESTAMPTZ NOT NULL
 );
