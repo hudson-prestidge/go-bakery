2563975a946e674b30e71f1581f0c717c3229a8b
Hudson Burns Prestidge <hudson.prestidge@gmail.com>
2019-04-09 06:42:39 +1200

    users can view products in cart

--- Client/cart.ts
+++ Client/cart.ts
@@ -1,16 +1,63 @@
 window.onload = function() {
-  getCartProducts()
+  retrieveCartProducts(setupCartList)
 }
 
-const getCartProducts = function () {
+interface Product {
+  Id: number,
+  Name: string,
+  Price: number
+}
+
+const retrieveCartProducts = function (callback?: (products:Product[], itemQuantities:{[Id:number] : number}) => void) :void {
   const getCart = new XMLHttpRequest
   getCart.open("GET", "/api/v1/users/cart")
   getCart.onload = function() {
     const cartData = JSON.parse(this.response)
-    console.log(cartData)
+    const items = cartData.Items
+
+    const products = cartData.Products
+    const itemQuantities:{[Id:number] : number} = {}
+    products.forEach(function(p :Product){
+      itemQuantities[p.Id] = 0
+    })
+    items.forEach(function(n :number) {
+      itemQuantities[n] += 1
+    })
+    callback(products, itemQuantities)
   }
   getCart.onerror = function(err) {
     console.log(err)
   }
   getCart.send()
+}
+
+const setupCartList = function(products:Product[], itemQuantities:{[Id:number] : number}) {
+  const cartList = document.querySelector("#cart-list")
+  let subtotalPrice :number = 0;
+  products.forEach(function(p:Product) {
+    let productRow = document.createElement("tr")
+    productRow.classList.add("product-row")
+    let productName = document.createElement("td")
+    productName.textContent = p.Name
+    let productQuantity = document.createElement("td")
+    productQuantity.textContent = `${itemQuantities[p.Id]}`
+    let productPrice = document.createElement("td")
+    productPrice.textContent = `$${(p.Price/100 * itemQuantities[p.Id]).toFixed(2)}`
+    subtotalPrice += (p.Price/100 * itemQuantities[p.Id])
+    productRow.appendChild(productName)
+    productRow.appendChild(productQuantity)
+    productRow.appendChild(productPrice)
+    cartList.appendChild(productRow)
+  })
+  let totalRow = document.createElement("tr")
+  totalRow.classList.add("total-row")
+  let namePlaceholder = document.createElement("td")
+  namePlaceholder.textContent = "total:"
+  let quantityPlaceholder = document.createElement("td")
+  let totalPrice = document.createElement("td")
+  totalPrice.textContent = `$${subtotalPrice.toFixed(2)}`
+  totalRow.appendChild(namePlaceholder)
+  totalRow.appendChild(quantityPlaceholder)
+  totalRow.appendChild(totalPrice)
+  cartList.appendChild(totalRow)
 }
--- Client/static/scripts/cart.js
+++ Client/static/scripts/cart.js
@@ -1,16 +1,55 @@
 window.onload = function () {
-    getCartProducts();
+    retrieveCartProducts(setupCartList);
 };
-const getCartProducts = function () {
+const retrieveCartProducts = function (callback) {
     const getCart = new XMLHttpRequest;
     getCart.open("GET", "/api/v1/users/cart");
     getCart.onload = function () {
         const cartData = JSON.parse(this.response);
-        console.log(cartData);
+        const items = cartData.Items;
+        const products = cartData.Products;
+        const itemQuantities = {};
+        products.forEach(function (p) {
+            itemQuantities[p.Id] = 0;
+        });
+        items.forEach(function (n) {
+            itemQuantities[n] += 1;
+        });
+        callback(products, itemQuantities);
     };
     getCart.onerror = function (err) {
         console.log(err);
     };
     getCart.send();
+};
+const setupCartList = function (products, itemQuantities) {
+    const cartList = document.querySelector("#cart-list");
+    let subtotalPrice = 0;
+    products.forEach(function (p) {
+        let productRow = document.createElement("tr");
+        productRow.classList.add("product-row");
+        let productName = document.createElement("td");
+        productName.textContent = p.Name;
+        let productQuantity = document.createElement("td");
+        productQuantity.textContent = `${itemQuantities[p.Id]}`;
+        let productPrice = document.createElement("td");
+        productPrice.textContent = `$${(p.Price / 100 * itemQuantities[p.Id]).toFixed(2)}`;
+        subtotalPrice += (p.Price / 100 * itemQuantities[p.Id]);
+        productRow.appendChild(productName);
+        productRow.appendChild(productQuantity);
+        productRow.appendChild(productPrice);
+        cartList.appendChild(productRow);
+    });
+    let totalRow = document.createElement("tr");
+    totalRow.classList.add("total-row");
+    let namePlaceholder = document.createElement("td");
+    namePlaceholder.textContent = "total:";
+    let quantityPlaceholder = document.createElement("td");
+    let totalPrice = document.createElement("td");
+    totalPrice.textContent = `$${subtotalPrice.toFixed(2)}`;
+    totalRow.appendChild(namePlaceholder);
+    totalRow.appendChild(quantityPlaceholder);
+    totalRow.appendChild(totalPrice);
+    cartList.appendChild(totalRow);
 };
 //# sourceMappingURL=cart.js.map
