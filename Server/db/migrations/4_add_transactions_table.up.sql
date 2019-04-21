d9f8a17782b0d1416c596d1aeb6eb7cf11cd72a0
Hudson Burns Prestidge <hudson.prestidge@gmail.com>
2019-04-21 16:40:26 +1200

    improve mobile responsiveness

--- Server/db/migrations/4_add_transactions_table.up.sql
+++ Server/db/migrations/4_add_transactions_table.up.sql
@@ -1,6 +1,5 @@
 CREATE TABLE transactions (
   id SERIAL PRIMARY KEY,
   user_id INTEGER NOT NULL REFERENCES users(id),
-  product_id INTEGER NOT NULL REFERENCES products(id),
-  quantity INTEGER NOT NULL
+  products INTEGER[] NOT NULL
 );
